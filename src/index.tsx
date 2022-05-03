e1ad024299178a26654eed3b1b68a8530bf2dbcf
Dmytro Vasyliev <dvcodetime@gmail.com>
2022-05-03 19:01:13 +0200

    Install sass package, add initial styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import { AppProviders } from 'providers/AppProviders';
-
-import { App } from './app/App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from "react-dom";
+import { AppProviders } from "providers/AppProviders";
+import { App } from "./app/App";
+import * as serviceWorker from "./serviceWorker";
+import "./styles/main.scss";
 
 ReactDOM.render(
   <AppProviders>
     <App />
   </AppProviders>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
