b2965bc3161f390e6b718daec96673c81c3279f9
Dmytro Vasyliev <dvcodetime@gmail.com>
2022-05-04 16:51:14 +0200

    Create Products page component

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,12 +1,86 @@
-import { Link } from "react-router-dom";
-import { AppRoute } from "routing/AppRoute.enum";
+import { Navbar, Product } from "components";
 
 const Products = () => {
   return (
-    <>
-      <h2>Products page</h2>
-      <Link to={AppRoute.Login}> Login </Link>
-    </>
+    <section className="products">
+      <Navbar className="products__navbar" />
+
+      <div className="products__container">
+        <div className="products__items">
+          <Product
+            active={true}
+            promo={true}
+            image="https://picsum.photos/640/480"
+            name="Product name"
+            description="Lorem ipsum dolor sit, amet consectetur adipisicing elit."
+            rating={5}
+          />
+
+          <Product
+            active={true}
+            promo={false}
+            image="https://picsum.photos/640/480"
+            name="Product name"
+            description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, illum"
+            rating={0}
+          />
+
+          <Product
+            active={false}
+            promo={true}
+            image="https://picsum.photos/640/480"
+            name="Product name"
+            description="Lorem ipsum dolor sit amet."
+            rating={1}
+          />
+
+          <Product
+            active={false}
+            promo={false}
+            image="https://picsum.photos/640/480"
+            name="Product name"
+            description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, illum"
+            rating={3}
+          />
+
+          <Product
+            active={false}
+            promo={true}
+            image="https://picsum.photos/640/480"
+            name="Product name"
+            description="Lorem ipsum dolor sit amet."
+            rating={1}
+          />
+
+          <Product
+            active={false}
+            promo={false}
+            image="https://picsum.photos/640/480"
+            name="Product name"
+            description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, illum"
+            rating={3}
+          />
+
+          <Product
+            active={true}
+            promo={true}
+            image="https://picsum.photos/640/480"
+            name="Product name"
+            description="Lorem ipsum dolor sit, amet consectetur adipisicing elit."
+            rating={5}
+          />
+
+          <Product
+            active={true}
+            promo={false}
+            image="https://picsum.photos/640/480"
+            name="Product name"
+            description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime, illum"
+            rating={0}
+          />
+        </div>
+      </div>
+    </section>
   );
 };
 
